1fdceb261d8e55a0314f662674e19abb50826d08
Valentin B <valbou@valbou.fr>
2023-02-05 08:46:12 +0100

    Fix typo

---
+++ tests/base/http/flask/test_routes.py
@@ -0,0 +1,14 @@
+from tests import FlaskTestCase, FlaskTemplateCapture
+
+
+class TestBaseRoutes(FlaskTestCase):
+    def test_home_page(self):
+        recorder = FlaskTemplateCapture(self.app)
+        with recorder:
+            response = self.client.get("/")
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.content_type, "text/html; charset=utf-8")
+            self.assertTemplateUsed("home.html", recorder.get_recorded_templates())
+
+        self.assertIn("<title>vTasks API</title>", response.text)
+        self.assertIn("vtasks-logo-light.svg", response.text)
