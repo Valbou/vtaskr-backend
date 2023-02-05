b3431eff333a6ca78090a22a55af3fe709b25df1
Valentin B <valbou@valbou.fr>
2023-02-05 15:47:18 +0100

    Add test on routes

--- tests/flask/test_flask.py
+++ tests/flask/test_flask.py
@@ -7,5 +7,5 @@ from vtasks.flask.main import create_flask_app
 
 class TestCreateFlaskApp(TestCase):
     def test_create_app(self):
-        app = create_flask_app()
+        app = create_flask_app(testing=True)
         self.assertIsInstance(app, Flask)
