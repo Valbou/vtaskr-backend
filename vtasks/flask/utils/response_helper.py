660bddccfb2b739a9db7bfbe8400d46c0b591a25
Valentin B <valbou@valbou.fr>
2023-02-05 18:40:18 +0100

    Add response API helper

--- vtasks/flask/utils/response_helper.py
+++ vtasks/flask/utils/response_helper.py
@@ -11,26 +11,27 @@ class ResponseAPI:
     @classmethod
     def get_response(cls, data: Any, status: int, headers: Optional[dict] = None):
         headers = headers or {}
-        assert status < 400, f"A status >= 400 is an error not a normal response, given: {status}"
+        assert (  # nosec
+            status < 400
+        ), f"A status >= 400 is an error not a normal response, given: {status}"
         return Response(
             response=dumps(data),
             status=status,
             headers=headers,
             mimetype=JSON_MIME_TYPE,
-            content_type=JSON_MIME_TYPE
+            content_type=JSON_MIME_TYPE,
         )
 
     @classmethod
-    def get_error_response(cls, message: str, status: int, headers: Optional[dict] = None):
-        data = {
-            "error": message,
-            "status": status
-        }
+    def get_error_response(
+        cls, message: str, status: int, headers: Optional[dict] = None
+    ):
+        data = {"error": message, "status": status}
         headers = headers or {}
         return Response(
             response=dumps(data),
             status=status,
             headers=headers,
             mimetype=JSON_MIME_TYPE,
-            content_type=JSON_MIME_TYPE
+            content_type=JSON_MIME_TYPE,
         )
