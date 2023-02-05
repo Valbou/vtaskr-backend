406f12e5394641687c6e491ff5b8c92ca141dbb1
Valentin B <valbou@valbou.fr>
2023-02-05 06:52:16 +0100

    Add assertTemplateUsed

--- tests/flask/test_flask.py
+++ tests/flask/test_flask.py
@@ -1,10 +1,14 @@
-from tests.base_http_test import FlaskTestCase
+from tests.base_http_test import FlaskTestCase, FlaskTemplateCapture
 
 
 class TestFlask(FlaskTestCase):
     def test_home_page(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+        recorder = FlaskTemplateCapture(self.app)
+        with recorder:
+            response = self.client.get("/")
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.content_type, "text/html; charset=utf-8")
+            self.assertTemplateUsed("home.html", recorder.get_recorded_templates())
+
         self.assertIn("<title>vTask API</title>", response.text)
         self.assertIn("vtasks-logo-light.svg", response.text)
