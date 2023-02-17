b9162b62af0bc2445acc31390876a8309a54a44b
Valentin B <valbou@valbou.fr>
2023-02-17 07:34:53 +0100

    #3 Fix clean token expired

--- tests/users/hmi/flask/test_email_content.py
+++ tests/users/hmi/flask/test_email_content.py
@@ -1,6 +1,6 @@
 from tests import BaseTestCase, FlaskTemplateCapture
 
-from vtasks.users.hmi.flask.email_content import RegisterEmail
+from vtasks.users.hmi.flask.email_content import RegisterEmail, LoginEmail
 
 
 class TestRegisterEmail(BaseTestCase):
@@ -25,4 +25,33 @@ class TestRegisterEmail(BaseTestCase):
                 self.assertIn(self.first_name, email.html)
                 self.assertIn("data:image/svg+xml;base64,", email.html)
                 self.assertIn(self.first_name, email.text)
+                self.assertIn(self.to, email.to_emails)
+
+
+class TestLoginEmail(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.to = self.fake.email(domain="valbou.fr")
+        self.first_name = self.fake.first_name()
+        self.last_name = self.fake.last_name()
+        self.code = self.fake.password()
+
+    def test_email_object(self):
+        with self.app.app_context():
+            recorder = FlaskTemplateCapture(self.app)
+            with recorder:
+                email = LoginEmail(
+                    to=[self.to],
+                    first_name=self.first_name,
+                    last_name=self.last_name,
+                    code=self.code,
+                )
+                self.assertTemplateUsed(
+                    "emails/simple_text.html", recorder.get_recorded_templates()
+                )
+                self.assertIn(self.first_name, email.html)
+                self.assertIn(self.code, email.html)
+                self.assertIn("data:image/svg+xml;base64,", email.html)
+                self.assertIn(self.first_name, email.text)
+                self.assertIn(self.code, email.text)
                 self.assertIn(self.to, email.to_emails)
