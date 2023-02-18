7263e4f9b4cffed48bb970ef795105f0b004c49a
Valentin B <valbou@valbou.fr>
2023-02-18 07:44:33 +0100

    Fix typing and useless return

--- vtasks/tasks/persistence/sqlalchemy/adapters/tags.py
+++ vtasks/tasks/persistence/sqlalchemy/adapters/tags.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
@@ -6,22 +8,20 @@ from vtasks.tasks.models import Tag
 
 
 class TagDB(AbstractTagPort):
-    def load(self, session: Session, id: str) -> Tag:
+    def load(self, session: Session, id: str) -> Optional[Tag]:
         stmt = select(Tag).where(Tag.id == id)
         result = session.scalars(stmt).one_or_none()
         return result
 
-    def save(self, session: Session, tag: Tag, autocommit: bool = True) -> True:
+    def save(self, session: Session, tag: Tag, autocommit: bool = True):
         session.add(tag)
         if autocommit:
             session.commit()
-        return True
 
-    def delete(self, session: Session, tag: Tag, autocommit: bool = True) -> True:
+    def delete(self, session: Session, tag: Tag, autocommit: bool = True):
         session.delete(tag)
         if autocommit:
             session.commit()
-        return True
 
     def exists(self, session: Session, id: str) -> bool:
         return session.query(select(Tag).where(Tag.id == id).exists()).scalar()
