0e7c466b1767f4ae6ac13478e98f2ea46c6f1319
Valentin B <valbou@valbou.fr>
2023-02-28 19:45:21 +0100

    #2 change strategy to translate emails

--- vtasks/users/hmi/flask/email_content.py
+++ vtasks/users/hmi/flask/email_content.py
@@ -16,25 +16,23 @@ class RegisterEmail(AbstractBaseEmailContent):
             "emails/simple_text.html", **self.email_context(first_name)
         )
         self.to_emails = to
-        self.text = self._(
-            """
-                Welcome {first_name} !
-                Welcome in your new to do app. As of now, you can start to create tasks !
-            """
-        ).format(first_name=first_name)
+        self.text = f"""
+{self._("Welcome {first_name} !")}
+{self._("Welcome in your new to do app. As of now, you can start to create tasks !")}
+        """.format(
+            first_name=first_name
+        )
 
     def email_context(self, first_name: str) -> dict:
         title = self._("Registration Success {first_name}").format(
             first_name=first_name
         )
         content_title = self._("Welcome {first_name} !").format(first_name=first_name)
-        content = self._(
-            """
-                <p>
-                    Welcome in your new to do app. As of now, you can start to create tasks !
-                </p>
-            """
-        )
+        content = f"""
+<p>
+    {self._("Welcome in your new to do app. As of now, you can start to create tasks !")}
+</p>
+        """
         call_to_action = self._("Enjoy !")
 
         context = {
@@ -58,31 +56,36 @@ class LoginEmail(AbstractBaseEmailContent):
             "emails/simple_text.html", **self.email_context(first_name, code)
         )
         self.to_emails = to
-        self.text = self._(
-            """
-Hi {first_name} !
-To finish login process (2FA), please copy/paste the following code: {code}
-This code stay valid only three minutes, after, you need to make a new login attempt.
-            """
-        ).format(first_name=first_name, code=code)
+        self.text = f"""
+{self._("Hi {first_name} !")}
+{self._("To finish login process (2FA), please copy/paste the following code: {code}")}
+{self._(
+    "This code stay valid only three minutes, after,"
+    "you need to make a new login attempt."
+)}
+        """.format(
+            first_name=first_name, code=code
+        )
 
     def email_context(self, first_name: str, code: str) -> dict:
         title = self._("New login with your account")
         content_title = self._("Hi {first_name} !").format(first_name=first_name)
-        content = self._(
-            """
-                <p>
-                    To finish login process (2FA), please copy/paste the following code:
-                </p>
-                <p style="font-size: 16px: font-weight: bolder; letter-spacing: 5px;">
-                    {code}
-                </p>
-                <p>
-                    This code stay valid only three minutes, after,
-                    you need to make a new login attempt.
-                </p>
-            """
-        ).format(code=code)
+        content = f"""
+<p>
+    {self._("To finish login process (2FA), please copy/paste the following code:")}
+</p>
+<p style="font-size: 16px: font-weight: bolder; letter-spacing: 5px;">
+    {"{code}"}
+</p>
+<p>
+    {self._(
+        "This code stay valid only three minutes, after,"
+        "you need to make a new login attempt."
+    )}
+</p>
+        """.format(
+            code=code
+        )
 
         context = {
             "logo": self.logo,
@@ -105,34 +108,39 @@ class ChangeEmailToOldEmail(AbstractBaseEmailContent):
             "emails/simple_text.html", **self.email_context(first_name, code)
         )
         self.to_emails = to
-        self.text = self._(
-            """
-Hi {first_name} !
-You request us to change your email account.
-We have sent to you an email, in your new email account.
-Click on the email sent to your new account and copy/paste the following code into the form:
-{code}
-This old email will be remove of your account after proceed.
-            """
-        ).format(first_name=first_name, code=code)
+        self.text = f"""
+{self._("Hi {first_name} !")}
+{self._("You request us to change your email account.")}
+{self._("We have sent to you an email, in your new email account.")}
+{self._(
+    "Click on the email sent to your new account and"
+    "copy/paste the following code into the form:"
+)}
+{"{code}"}
+{self._("This old email will be remove of your account after proceed.")}
+        """.format(
+            first_name=first_name, code=code
+        )
 
     def email_context(self, first_name: str, code: str) -> dict:
         title = self._("Change your Email").format(first_name=first_name)
         content_title = self._("Hi {first_name} !").format(first_name=first_name)
-        content = self._(
-            """
-                <p>
-                    You request us to change your email account.
-                    We have sent to you an email, in your new email account.<br />
-                    Click on the email sent to your new account and copy/paste the
-                    following code into the form:
-                </p>
-                <p style="font-size: 16px: font-weight: bolder; letter-spacing: 5px;">
-                    {code}
-                </p>
-                <p>This old email will be removed of your account after proceed.</p>
-            """
-        ).format(code=code)
+        content = f"""
+<p>
+    {self._("You request us to change your email account.")}
+    {self._("We have sent to you an email, in your new email account.")}<br />
+    {self._(
+        "Click on the email sent to your new account and"
+        "copy/paste the following code into the form:"
+    )}
+</p>
+<p style="font-size: 16px: font-weight: bolder; letter-spacing: 5px;">
+    {"{code}"}
+</p>
+<p>{self._("This old email will be remove of your account after proceed.")}</p>
+            """.format(
+            code=code
+        )
 
         context = {
             "logo": self.logo,
@@ -162,30 +170,36 @@ class ChangeEmailToNewEmail(AbstractBaseEmailContent):
             **self.email_context(first_name, change_email_link),
         )
         self.to_emails = to
-        self.text = self._(
-            """
-Hi {first_name} !
-You request us to change your email account.
-We sent you an email, in your old email account with a code required to complete the process.
-Follow the link below to proceed:
-{change_email_link}
-            """
-        ).format(first_name=first_name, change_email_link=change_email_link)
+        self.text = f"""
+{self._("Hi {first_name} !")}
+{self._("You request us to change your email account.")}
+{self._(
+    "We sent you an email, in your old email account"
+    "with a code required to complete the process."
+)}
+{self._("Follow the link below to proceed")} {self._("(link available only 3 minutes)")}:
+{self._("{change_email_link}")}
+{self._("This old email will be removed of your account after proceed.")}
+        """.format(
+            first_name=first_name, change_email_link=change_email_link
+        )
 
     def email_context(self, first_name: str, change_email_link: str) -> dict:
         title = self._("Change your Email").format(first_name=first_name)
         content_title = self._("Hi {first_name} !").format(first_name=first_name)
-        content = self._(
-            """
-                <p>
-                    You request us to change your email account.
-                    We sent you an email, in your old email account with a code
-                    required to complete the process.<br />
-                    Click on the following button to proceed (link available only 3 minutes).
-                </p>
-                <p>This old email will be removed of your account after proceed.</p>
-            """
-        )
+        content = f"""
+<p>
+    {self._("You request us to change your email account.")}
+    {self._(
+        "We sent you an email, in your old email account"
+        "with a code required to complete the process."
+    )}
+    <br />
+    {self._("Click on the following button to proceed")}
+    {self._("(link available only 3 minutes)")}.
+</p>
+<p>{self._("This old email will be removed of your account after proceed.")}</p>
+        """
 
         context = {
             "logo": self.logo,
@@ -216,26 +230,25 @@ class ChangePasswordEmail(AbstractBaseEmailContent):
             **self.email_context(first_name, change_email_link),
         )
         self.to_emails = to
-        self.text = self._(
-            """
-Hi {first_name} !
-You request us to change your account password.
-Follow the link below to proceed:
-{change_email_link}
-            """
-        ).format(first_name=first_name, change_email_link=change_email_link)
+        self.text = f"""
+{self._("Hi {first_name} !")}
+{self._("You request us to change your account password.")}
+{self._("Follow the link below to proceed")}
+{self._("{change_email_link}")}
+        """.format(
+            first_name=first_name, change_email_link=change_email_link
+        )
 
     def email_context(self, first_name: str, change_email_link: str) -> dict:
         title = self._("Change your Email").format(first_name=first_name)
         content_title = self._("Hi {first_name} !").format(first_name=first_name)
-        content = self._(
-            """
-                <p>
-                    You request us to change your account password.
-                    Follow the link below to proceed (link available only 3 minutes).
-                </p>
-            """
-        )
+        content = f"""
+<p>
+    {self._("You request us to change your account password.")}
+    {self._("Click on the following button to proceed")}
+    {self._("(link available only 3 minutes)")}.
+</p>
+        """
 
         context = {
             "logo": self.logo,
