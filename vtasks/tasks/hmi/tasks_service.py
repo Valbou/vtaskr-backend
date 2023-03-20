dc2f13a76b75c1cb00ccca65ed8ba36b3c076bff
Valentin B <valbou@valbou.fr>
2023-03-20 19:37:08 +0100

    Task moved to DTO

--- vtasks/tasks/hmi/tasks_service.py
+++ vtasks/tasks/hmi/tasks_service.py
@@ -12,5 +12,4 @@ class TaskService(AbstractTaskPort):
         self.task_db = TaskDB()
 
     def get_user_tasks(self, user_id: str) -> List[dict]:
-        tasks = self.task_db.user_tasks(self.session, user_id)
-        return [t.to_external_data() for t in tasks]
+        return self.task_db.user_tasks(self.session, user_id)
