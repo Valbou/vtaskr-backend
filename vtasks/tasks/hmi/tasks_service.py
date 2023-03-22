87148ddc26be9fc36b5756debf397505c0dd7737
Valentin B <valbou@valbou.fr>
2023-03-22 21:01:25 +0100

    Add tasks endpoints

--- vtasks/tasks/hmi/tasks_service.py
+++ vtasks/tasks/hmi/tasks_service.py
@@ -16,6 +16,16 @@ class TaskService(AbstractTaskPort):
         return self.task_db.user_tasks(self.session, user_id)
 
     def get_user_task(self, user_id: str, task_id: str) -> Optional[Task]:
-        # TODO: Add a better security
+        # TODO: Add a better security management
         task = self.task_db.load(self.session, task_id)
         return task if task.user_id == user_id else None
+
+    def update_user_task(self, user_id: str, task: Task):
+        # TODO: Add a better security management
+        if task.user_id == user_id:
+            self.task_db.save(self.session, task)
+
+    def delete_user_task(self, user_id: str, task: Task):
+        # TODO: Add a better security management
+        if task.user_id == user_id:
+            self.task_db.delete(self.session, task)
