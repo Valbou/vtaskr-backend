c04276520091191fef6700f2dcc77168b1c8c2db
Valbou <Valbou>
2023-03-25 10:52:49 +0100

    #4 add tests

---
+++ tests/users/models/test_request_change.py
@@ -0,0 +1,106 @@
+from unittest import TestCase
+from datetime import datetime, timedelta
+from typing import Optional
+from pytz import utc
+
+from faker import Faker
+
+from vtasks.base.config import REQUEST_VALIDITY, REQUEST_DAYS_HISTORY
+from vtasks.users import RequestChange, RequestType
+
+
+class TestRequestChange(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.fake = Faker()
+
+    def create_request_change(
+        self, type: RequestType = RequestType.PASSWORD, email: Optional[str] = None
+    ) -> RequestChange:
+        email = email or self.fake.email(domain="valbou.fr")
+        return RequestChange(type, email)
+
+    def test_request_change_table_fields(self):
+        self.assertEqual(RequestChange.__annotations__.get("id"), str)
+        self.assertEqual(RequestChange.__annotations__.get("created_at"), datetime)
+        self.assertEqual(
+            RequestChange.__annotations__.get("request_type"), Optional[RequestType]
+        )
+        self.assertEqual(RequestChange.__annotations__.get("email"), str)
+        self.assertEqual(RequestChange.__annotations__.get("code"), str)
+        self.assertEqual(RequestChange.__annotations__.get("done"), bool)
+
+    def test_request_change_is_valid(self):
+        request_change = self.create_request_change()
+        self.assertTrue(request_change.is_valid())
+
+    def test_request_change_invalid_if_done(self):
+        request_change = self.create_request_change()
+        request_change.done = True
+        self.assertFalse(request_change.is_valid())
+
+    def test_request_change_old_invalid(self):
+        request_change = self.create_request_change()
+        request_change.created_at = datetime.now(utc) - timedelta(
+            seconds=REQUEST_VALIDITY
+        )
+        self.assertFalse(request_change.is_valid())
+
+    def test_request_change_in_history(self):
+        request_change = self.create_request_change()
+        request_change.created_at = datetime.now(utc) - timedelta(
+            seconds=REQUEST_VALIDITY
+        )
+        self.assertFalse(
+            request_change.created_at < RequestChange.history_expired_before()
+        )
+
+    def test_request_change_out_of_history(self):
+        request_change = self.create_request_change()
+        request_change.created_at = datetime.now(utc) - timedelta(
+            days=REQUEST_DAYS_HISTORY
+        )
+        self.assertTrue(
+            request_change.created_at < RequestChange.history_expired_before()
+        )
+
+    def test_valid_after(self):
+        request_change = self.create_request_change()
+        self.assertTrue(request_change.created_at > RequestChange.valid_after())
+
+    def test_invalid_after(self):
+        request_change = self.create_request_change()
+        request_change.created_at = datetime.now(utc) - timedelta(
+            seconds=REQUEST_VALIDITY
+        )
+        self.assertFalse(request_change.created_at > RequestChange.valid_after())
+
+    def test_check_code(self):
+        request_change = self.create_request_change()
+        self.assertIsInstance(request_change.code, str)
+        self.assertTrue(request_change.check_code(request_change.code))
+        self.assertFalse(request_change.check_code(self.fake.bothify("###???")))
+
+    def test_consistency_gen_hash(self):
+        request_change = self.create_request_change()
+        self.assertIsInstance(request_change.gen_hash(), str)
+        self.assertEqual(len(request_change.gen_hash()), 64)
+        self.assertEqual(request_change.gen_hash(), request_change.gen_hash())
+        self.assertTrue(request_change.check_hash(request_change.gen_hash()))
+
+        request_change_2 = self.create_request_change()
+        self.assertNotEqual(request_change.gen_hash(), request_change_2.gen_hash())
+        self.assertFalse(request_change.check_hash(request_change_2.gen_hash()))
+
+    def test_set_email(self):
+        request_change = self.create_request_change()
+        email = request_change.email
+        self.assertTrue(request_change.set_email(self.fake.email(domain="valbou.fr")))
+        self.assertNotEqual(email, request_change.email)
+        self.assertFalse(request_change.set_email(self.fake.word()))
+
+    def test_mark_as_done(self):
+        request_change = self.create_request_change()
+        self.assertFalse(request_change.done)
+        request_change.mark_as_done()
+        self.assertTrue(request_change.done)
---
+++ tests/users/persitence/sqlalchemy/test_request_change_table.py
@@ -0,0 +1,85 @@
+from datetime import datetime, timedelta
+
+from tests import BaseTestCase
+
+from vtasks.base.config import REQUEST_DAYS_HISTORY
+from vtasks.users.models import RequestChange, RequestType
+from vtasks.users.persistence import RequestChangeDB
+
+
+class TestRequestChangeTable(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.table_name = "requestschanges"
+        self.columns_name = [
+            "id",
+            "created_at",
+            "request_type",
+            "email",
+            "code",
+            "done",
+        ]
+
+    def test_table_exists(self):
+        self.assertTableExists(self.table_name)
+
+    def test_columns_exists(self):
+        self.assertColumnsExists(self.table_name, self.columns_name)
+
+
+class TestRequestChangeAdapter(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.request_change_db = RequestChangeDB()
+        self.request_change = RequestChange(
+            RequestType.EMAIL, email=self.fake.email(domain="valbou.fr")
+        )
+
+    def test_complete_crud_request_change(self):
+        with self.app.sql_service.get_session() as session:
+            self.assertIsNone(
+                self.request_change_db.load(session, self.request_change.id)
+            )
+            self.request_change_db.save(session, self.request_change)
+            self.assertTrue(
+                self.request_change_db.exists(session, self.request_change.id)
+            )
+            old_code = self.request_change.code
+            self.request_change.code = "abc"  # nosec
+            session.commit()
+            request_change = self.request_change_db.load(
+                session, self.request_change.id
+            )
+            self.assertNotEqual(old_code, request_change.code)
+            self.assertEqual(request_change.id, self.request_change.id)
+            self.request_change_db.delete(session, self.request_change)
+            self.assertFalse(
+                self.request_change_db.exists(session, self.request_change.id)
+            )
+
+    def test_in_history(self):
+        with self.app.sql_service.get_session() as session:
+            self.request_change_db.save(session, self.request_change)
+            self.assertTrue(
+                self.request_change_db.exists(session, self.request_change.id)
+            )
+            self.request_change_db.clean_history(session)
+            self.assertTrue(
+                self.request_change_db.exists(session, self.request_change.id)
+            )
+
+    def test_not_in_history(self):
+        self.request_change.created_at = datetime.now() - timedelta(
+            days=REQUEST_DAYS_HISTORY
+        )
+
+        with self.app.sql_service.get_session() as session:
+            self.request_change_db.save(session, self.request_change)
+            self.assertTrue(
+                self.request_change_db.exists(session, self.request_change.id)
+            )
+            self.request_change_db.clean_history(session)
+            self.assertFalse(
+                self.request_change_db.exists(session, self.request_change.id)
+            )
