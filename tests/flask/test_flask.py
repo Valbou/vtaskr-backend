ae473efe68bb65274cdf2d2f65afcdde9cabf9fb
Valbou <Valbou>
2023-03-25 10:52:49 +0100

    Reorganize flask base items

--- tests/flask/test_flask.py
+++ tests/flask/test_flask.py
@@ -1,14 +1,11 @@
-from tests.base_http_test import FlaskTestCase, FlaskTemplateCapture
+from unittest import TestCase
+
+from flask import Flask
+
+from vtasks.flask.main import create_flask_app
 
 
-class TestFlask(FlaskTestCase):
-    def test_home_page(self):
-        recorder = FlaskTemplateCapture(self.app)
-        with recorder:
-            response = self.client.get("/")
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.content_type, "text/html; charset=utf-8")
-            self.assertTemplateUsed("home.html", recorder.get_recorded_templates())
-
-        self.assertIn("<title>vTask API</title>", response.text)
-        self.assertIn("vtasks-logo-light.svg", response.text)
+class TestCreateFlaskApp(TestCase):
+    def test_create_app(self):
+        app = create_flask_app()
+        self.assertIsInstance(app, Flask)
