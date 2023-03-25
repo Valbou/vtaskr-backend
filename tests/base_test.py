18fe73f3c8eeff0c57e3723049152c288aa840f7
Valbou <Valbou>
2023-03-25 10:52:49 +0100

    Change to BaseTestCase including flask and sqlalchemy

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,13 +1,40 @@
-from .base_db_test import DBTestCase
-from .base_http_test import FlaskTestCase
+from unittest import TestCase
+from typing import List
+
+from faker import Faker
+from flask import Flask, template_rendered
+
+from vtasks.flask.main import create_flask_app
+from vtasks.sqlalchemy.database import SQLService
 
 
-class BaseTestCase(FlaskTestCase, DBTestCase):
+class FlaskTemplateCapture:
+    def __init__(self, app: Flask) -> None:
+        self.recorded_templates: List[str] = []
+        self.app = app
+
+    def __enter__(self):
+        template_rendered.connect(self.add, self.app)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        template_rendered.disconnect(self.add, self.app)
+
+    def add(self, sender, template, context, **extra):
+        self.recorded_templates.append(template.name or "string template")
+
+    def get_recorded_templates(self):
+        return self.recorded_templates
+
+
+class BaseTestCase(TestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        if not self.defined_http_service:
-            self._define_client()
+        self.fake = Faker()
+        self.app: Flask = create_flask_app(testing=True)
+        self.client = self.app.test_client()
+        self.cli = self.app.test_cli_runner()
+        self.app.sql_service = SQLService(testing=True, echo=False)
 
-        if not self.defined_sql_service:
-            self._define_sql_service()
+    def assertTemplateUsed(self, template_name: str, recorded_templates: List[str]):
+        self.assertIn(template_name, recorded_templates)
--- tests/users/persitence/sqlalchemy/test_user_table.py
+++ tests/users/persitence/sqlalchemy/test_user_table.py
@@ -1,29 +1,23 @@
-from sqlalchemy.orm import Session
-
-from faker import Faker
-
-from tests import DBTestCase
+from tests import BaseTestCase
 from tests.utils.db_utils import text_query_table_exists
 
 from vtasks.users.models import User
 from vtasks.users.persistence import UserDB
-from vtasks.sqlalchemy.database import DBType
 
 
-class TestUserTable(DBTestCase):
+class TestUserTable(BaseTestCase):
     def test_users_table_exists(self):
-        with Session(self.sql_test.get_engine()) as session:
+        with self.app.sql_service.get_session() as session:
             result = session.execute(
                 text_query_table_exists(), params={"table": "users"}
             ).scalar_one_or_none()
             self.assertTrue(result)
 
 
-class TestUserAdapter(DBTestCase):
+class TestUserAdapter(BaseTestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.fake = Faker()
-        self.user_db = UserDB(DBType.TEST)
+        self.user_db = UserDB()
         self.user = User(
             first_name=self.fake.first_name(),
             last_name=self.fake.last_name(),
@@ -32,7 +26,7 @@ class TestUserAdapter(DBTestCase):
         )
 
     def test_complete_crud_user(self):
-        with self.user_db.get_session() as session:
+        with self.app.sql_service.get_session() as session:
             self.assertIsNone(self.user_db.load(session, self.user.id))
             self.assertTrue(self.user_db.save(session, self.user))
             self.assertTrue(self.user_db.exists(session, self.user.id))
@@ -44,3 +38,9 @@ class TestUserAdapter(DBTestCase):
             self.assertEqual(user.id, self.user.id)
             self.assertTrue(self.user_db.delete(session, self.user))
             self.assertFalse(self.user_db.exists(session, self.user.id))
+
+    def test_user_find_login(self):
+        with self.app.sql_service.get_session() as session:
+            self.assertTrue(self.user_db.save(session, self.user))
+            user = self.user_db.find_login(session, email=self.user.email)
+            self.assertEqual(user.first_name, self.user.first_name)
