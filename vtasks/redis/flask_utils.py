59e4eeb6afdbb820de4a8267acd7f764174a1a9c
Valbou <Valbou>
2023-03-25 10:52:50 +0100

    Optimize route with decorators

--- vtasks/redis/flask_utils.py
+++ vtasks/redis/flask_utils.py
@@ -1,21 +1,32 @@
 from datetime import timedelta
-from typing import Optional
+from typing import Optional, Callable
+from functools import wraps
+from logging import Logger
 
-from redis import Redis
-from flask import Request
+from flask import current_app, request, g
 
-from vtasks.flask.utils import get_ip
-from .ratelimit import RateLimit
+from vtasks.flask.utils import get_ip, ResponseAPI
+from vtasks.redis.ratelimit import RateLimit, LimitExceededError
 
 
-def rate_limit(
-    redis: Redis,
-    request: Request,
-    token: str,
-    hit: int = 1,
-    period: Optional[timedelta] = None,
-):
-    identifier = token or get_ip(request)
-    path = f"{request.method}:{request.path}"
+def rate_limited(logger: Logger, hit: int = 1, period: Optional[timedelta] = None):
     period = period or timedelta(seconds=1)
-    RateLimit(redis, identifier, path, hit, period)()
+
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            token = g.token if hasattr(g, "token") else None
+            identifier = token or get_ip(request)
+            path = f"{request.method}:{request.path}"
+
+            try:
+                RateLimit(
+                    current_app.nosql.get_engine(), identifier, path, hit, period
+                )()
+                return func(*args, **kwargs)
+            except LimitExceededError as e:
+                logger.warning(str(e))
+                return ResponseAPI.get_error_response("Too many requests", 429)
+
+        return wrapper
+    return decorator
---
+++ vtasks/users/hmi/flask/decorators.py
@@ -0,0 +1,31 @@
+from logging import Logger
+from typing import Callable
+from functools import wraps
+
+from flask import current_app, request, g
+
+from vtasks.flask.utils import ResponseAPI, get_bearer_token
+from .user_service import UserService
+
+
+def login_required(logger: Logger):
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                sha_token = get_bearer_token(request)
+                if not sha_token:
+                    return ResponseAPI.get_error_response("Invalid token", 401)
+
+                with current_app.sql.get_session() as session:
+                    session.expire_on_commit = False
+                    auth_service = UserService(session, testing=current_app.testing)
+                    user = auth_service.user_from_token(sha_token)
+                    g.token = sha_token
+                    g.user = user
+                return func(*args, **kwargs)
+            except Exception as e:
+                logger.error(str(e))
+                return ResponseAPI.get_error_response("Internal error", 500)
+        return wrapper
+    return decorator
