c6f6747f259277be27e88c97922d48add2772423
Valbou <Valbou>
2023-03-25 10:52:49 +0100

    Reorganize app.py

--- src/vtasks/app.py
+++ src/vtasks/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask
 
+from .apps.base import base_bp
 from .apps.users import users_bp
 from .apps.tasks import tasks_bp
 from .database import db_session
@@ -8,19 +9,7 @@ from .database import db_session
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    """Just a logo displayed to curious users"""
-    return render_template("home.html")
-
-
-@app.route("/tests", methods=["GET"])
-def tests():
-    """URL to test flask - REMOVE ME"""
-    # TODO: to remove when tests are done
-    return jsonify({"test_num": 456, "test_str": "test"})
-
-
+app.register_blueprint(base_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(tasks_bp)
 
