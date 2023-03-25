b16d897bc039341fd1327ef42b75164cb493447f
Valbou <Valbou>
2023-03-25 10:52:49 +0100

    Update ports

--- vtasks/users/persistence/ports/token_port.py
+++ vtasks/users/persistence/ports/token_port.py
@@ -9,3 +9,11 @@ class AbstractTokenPort(AbstractPort, ABC):
     @abstractmethod
     def get_token(self, token: str) -> Token:
         raise NotImplementedError()
+
+    @abstractmethod
+    def activity_update(self, token: str) -> Token:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def clean_expired(self) -> int:
+        raise NotImplementedError()
--- vtasks/users/persistence/ports/user_port.py
+++ vtasks/users/persistence/ports/user_port.py
@@ -9,3 +9,7 @@ class AbstractUserPort(AbstractPort, ABC):
     @abstractmethod
     def find_login(self, email: str) -> User:
         raise NotImplementedError()
+
+    @abstractmethod
+    def update(self, user: User) -> True:
+        raise NotImplementedError()
