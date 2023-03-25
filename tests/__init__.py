507eb869b99707b61776daac3a88012c6a10fba5
Valbou <Valbou>
2023-03-25 10:52:50 +0100

    Add redis cleanup when tests starts

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,17 @@
 from dotenv import load_dotenv
 
 from vtasks.sqlalchemy.database import SQLService
+from vtasks.redis.database import NoSQLService
 
 from .base_test import BaseTestCase, FlaskTemplateCapture
 
 
 load_dotenv()
+
+# Clean cache
+nosql_test = NoSQLService(testing=True)
+redis = nosql_test.get_engine()
+redis.flushdb()
 
 # Clean database
 print("Install Tables...")
