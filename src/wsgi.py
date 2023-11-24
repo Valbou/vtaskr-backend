d33d94aa1b1e8d4cfcc028f96497ce33cedee512
Valbou <contact@valbou.fr>
2023-11-24 21:46:55 +0100

    fix: wsgi

--- src/wsgi.py
+++ src/wsgi.py
@@ -4,14 +4,13 @@ import logging
 import os
 import sys
 
-from .flask import app as application
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(BASE_DIR)
 
 sys.path.append(BASE_DIR)
-
 logging.basicConfig(stream=sys.stderr)
+
+from .flask import app as application  # noqa E402
 
 application.secret_key = os.getenv(
     "SECRET_KEY", "YM/92:>Dhqv=7p8+ixY=By?4i(%TU5L;W+4=dboG="
