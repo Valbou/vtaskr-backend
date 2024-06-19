f0dada304cc6f9c76365d13d3efe6bd9dc8010bc
Valbou <contact@valbou.fr>
2024-06-19 21:26:02 +0200

    test: fix group

--- tests/users/models/test_group.py
+++ tests/users/models/test_group.py
@@ -11,11 +11,12 @@ class TestGroup(TestCase):
         super().setUp()
         self.fake = Faker()
         self.group = Group(
-            is_private=False,
             name=self.fake.word(),
+            is_private=False,
         )
 
     def test_table_fields(self):
         self.assertEqual(Group.__annotations__.get("id"), str | None)
         self.assertEqual(Group.__annotations__.get("name"), str)
         self.assertEqual(Group.__annotations__.get("created_at"), datetime | None)
+        self.assertEqual(Group.__annotations__.get("is_private"), bool)
