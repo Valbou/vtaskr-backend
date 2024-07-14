fb4e5cce4c78a07423a34b1efac925bd0d8478b3
Valbou <contact@valbou.fr>
2024-07-14 16:21:51 +0200

    fix: invitation DTO

--- src/users/hmi/dto/invitation.py
+++ src/users/hmi/dto/invitation.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 from src.libs.openapi.base import openapi
-from src.users.models import Invitation
+from src.users.models import Invitation, Group, RoleType
 
 INVITATION_COMPONENT = "#/components/schemas/Invitation"
 
@@ -26,19 +26,30 @@ class InvitationDTO:
     from_user_id: str = ""
     to_user_email: str = ""
     in_group_id: str = ""
+    in_group: Group | None = None
     with_roletype_id: str = ""
+    with_roletype: RoleType | None = None
     created_at: str = ""
 
 
 class InvitationMapperDTO:
     @classmethod
     def model_to_dto(cls, invitation: Invitation) -> InvitationDTO:
+        from src.users.hmi.dto import GroupMapperDTO, RoleTypeMapperDTO
+
+        group_dto = GroupMapperDTO().model_to_dto(invitation.in_group) if invitation.in_group else None
+        roletype_dto = (
+            RoleTypeMapperDTO().model_to_dto(invitation.with_roletype) if invitation.with_roletype else None
+        )
+
         return InvitationDTO(
             id=invitation.id,
             from_user_id=invitation.from_user_id,
             to_user_email=invitation.to_user_email,
             in_group_id=invitation.in_group_id,
+            in_group=group_dto,
             with_roletype_id=invitation.with_roletype_id,
+            with_roletype=roletype_dto,
             created_at=invitation.created_at.isoformat(),
         )
 
