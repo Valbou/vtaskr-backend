5c2ce9c91d7bd0adb26932ef41cb085f3e5c9f97
Valbou <contact@valbou.fr>
2024-09-03 11:41:10 +0200

    test: add senders tests

--- src/notifications/services/senders.py
+++ src/notifications/services/senders.py
@@ -1,6 +1,6 @@
 import requests
 
-from src.notifications.models import BaseEmailContent
+from src.notifications.models import BaseEmailContent, BaseTelegramContent
 from src.ports import AbstractMessage, AbstractSender, MessageType
 from src.settings import TELEGRAM_BOT
 
@@ -19,20 +19,24 @@ class EmailSender(AbstractSender):
 
     def send(self):
         self.multi_smtp.add_emails(emails=self.messages)
-        self.messages.clear()
 
         if self.multi_smtp.has_messages:
             self.multi_smtp.send_all()
+
+        self.messages.clear()
 
 
 class TelegramSender(AbstractSender):
     handle_message_types = [MessageType.TELEGRAM]
 
+    def __init__(self) -> None:
+        self.compatible_message_classes = [BaseTelegramContent]
+
     def send(self):
         for message in self.messages:
             url = f"https://api.telegram.org/bot{TELEGRAM_BOT}/sendMessage"
 
-            for to in message.to:
+            for to in message.to.split(","):
                 data = {"chat_id": to, "text": message.text}
                 requests.post(url, data)
 
